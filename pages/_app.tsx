66e04563da70b95ebeaf216d4b0b6a41b9c6f67b
jerryznr99 <jerryznr99@gmail.com>
2024-09-06 04:37:50 -0700

    changed favicon + title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Montserrat } from "@next/font/google";
+import Head from "next/head";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
@@ -10,8 +11,13 @@ const montserrat = Montserrat({
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className={montserrat.className}>
-      <Component {...pageProps} />
-    </div>
+    <>
+      <Head>
+        <title>Jerry Zhang</title>
+      </Head>
+      <div className={montserrat.className}>
+        <Component {...pageProps} />
+      </div>
+    </>
   );
 }
